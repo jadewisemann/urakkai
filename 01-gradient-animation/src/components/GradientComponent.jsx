9e3dbaf4ad760a8527dc9a2fd692cdf9a0835831
jadewisemann <jadewisemann@gmail.com>
2024-11-05 18:15:48 +0900

    refactor: use 'styled-components'

--- 01-gradient-animation/src/components/GradientComponent.jsx
+++ 01-gradient-animation/src/components/GradientComponent.jsx
@@ -1,7 +1,22 @@
-import './styles.css';
+import styled, { keyframes } from 'styled-components';
 
-const GradientComponent = () => {
-  return <div className="gradient-animation" />;
+
+const gradientShift = keyframes`
+  0% { background-position: 0% 50%; }
+  50% { background-position: 100% 50%; }
+  100% { background-position: 0% 50%; }
+`;
+
+const GradientDiv = styled.div`
+  width: 100%;
+  height: 300px;
+  background: ${(props) => `linear-gradient(270deg, ${props.colors.join(', ')})`};
+  background-size: 600% 600%;
+  animation: ${gradientShift} ${(props) => props.speed}s ease infinite;
+`;
+
+const GradientComponent = ({ speed = 10, colors = ['#ff7e5f', '#feb47b', '#86a8e7', '#91eae4'] }) =>  {
+  return <GradientDiv speed={speed} colors={colors} />;
 }
 
 export default GradientComponent;
